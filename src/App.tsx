cbdce94211d5eab1cbcb3b4b71d47d13297529f9
pingzhu <ping.zhu@microfocus.com>
2021-04-10 08:54:15 +0800

    formate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import {ProjectListScreen} from "screens";
+import { ProjectListScreen } from "screens";
 import "./App.css";
 
 function App() {
